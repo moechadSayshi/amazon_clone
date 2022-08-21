9744eff5bd5aa49cee5c5eac620bc216b7d069c5
moechadSayshi <mohitsinghwiththeglasses@protonmail.com>
2022-08-21 07:16:24 +0530

    builing an simple api using various dependecies(http,express,mongoose)

--- server/index.js
+++ server/index.js
@@ -1,1 +1,22 @@
 console.log("hello world!")
+
+const express = require('express');
+const { get } = require('http');
+// same import: package in dart
+
+
+const PORT = 3000;
+
+const app = express();
+
+//building an api
+
+app.get('/',(req,res)=>{
+res.json({name: "mohit"});
+
+});
+
+app.listen(PORT,"0.0.0.0" , () =>{
+
+    console.log(`connected at port ${PORT} `);
+});
